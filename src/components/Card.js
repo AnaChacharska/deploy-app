7d37158b809c25f31a46c3afac468cc8e43154fa
AnaChacharska <117314692+AnaChacharska@users.noreply.github.com>
2025-01-04 00:03:17 +0100

    Update Card.js

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
+// Card.js
 import styles from "./Card.module.css";
 import { useDarkMode } from "../contexts/DarkModeContext";
 import Link from "next/link";
 
-const Card = ({ item, onEdit, onDelete }) => {
+const Card = ({ item, onEdit, onDelete, uiState }) => {
     const { isDarkMode } = useDarkMode();
 
     return (
@@ -13,7 +14,7 @@ const Card = ({ item, onEdit, onDelete }) => {
                 className={styles["card-image"]}
             />
             <div className={styles["card-content"]}>
-                <Link href={`/quote/${item.id}`}>
+                <Link href={`/quote/${item.id}?page=${uiState.currentPage}`}>
                     <h2
                         className={styles["card-title"]}
                         dangerouslySetInnerHTML={{ __html: item.title }}
